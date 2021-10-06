30a1288ce359ba65a8817eb4f8c74ae619f03679
G S bharath <76478624+bharath19-gs@users.noreply.github.com>
2021-10-06 17:32:31 +0530

    update commit

--- Code_snippets/Excercise_01/BasicGates/Nand_gate.v
+++ Code_snippets/Excercise_01/BasicGates/Nand_gate.v
@@ -1,1 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.08.2021 23:27:59
+// Design Name: 
+// Module Name: Nand_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module Nand_gate(
+    input A,
+    input B,
+    output C
+    );
+        assign C =  ; // need to do the AND operation here
+    
+endmodule
